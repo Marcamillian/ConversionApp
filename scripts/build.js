910dfc46ac23eb92961c809c56c9dc5c4203ebf2
Marcamillian <marcamillian@gmail.com>
2017-12-03 12:10:38 +0000

    Outlining the steps to building the code to a dist folder

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,18 @@
 const fs = require('fs')
+const rimraf = require('rimraf')
 const dir = './dist';
 
 
 // remove the directory
-if(fs.existsSync(dir)) fs.rmdirSync(dir)
+console.log(`is the folder here - ${fs.existsSync(dir)}`)
 
-// make the new directory
-fs.mkdirSync(dir)
+// needs to use promises
+// delete the folder and everything inside (if needed) 
+// THEN - create the new folder
+// THEN - copy all of the contents of the src/server folder over to dist/server
+// THEN - copy all except the .js file to dist/client
+// THEN - browserify the client .js code
+// THEN - babel the browserified code
+// THEN - write the resulting file to dist/client
+
+
