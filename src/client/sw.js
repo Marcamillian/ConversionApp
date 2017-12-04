a96d5d11329afd91394e4422848c15730bc8d26f
Marc-Desktop <marcamillian@gmail.com>
2017-12-04 14:38:11 +0000

    Caching fetch requests for offline moments

--- src/client/sw.js
+++ src/client/sw.js
@@ -1,25 +1,40 @@
 console.log("Running SW code")
 
+const staticCacheName = "convapp-static-v1"
 const fakeRates = JSON.stringify({'USD': 1, 'EUR': 1, 'GBP':1, 'INR':1})
 
 self.addEventListener('install',(event)=>{
-    
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache)=>{
+            return cache.addAll([
+                '/',
+                'main.js',
+                '/rates'
+            ])
+        })
+    )
 })
 
 self.addEventListener('fetch', (event)=>{   // listening for calls to fetch
     
     const requestUrl = new URL(event.request.url)
     
+    // == Deal with the rates request
+    /*
     if (requestUrl.origin == location.origin){ // if the site we are getting from is our own
         if(requestUrl.pathname === '/rates'){   // if it is a call to the rates object
-            //event.respondWith(fetch(event.request))
             event.respondWith(promiseWrap(fakeRates))
             return;
         }
-    }
-    
-    event.respondWith(fetch(event.request)) // return what it originally wanted
-    return
+    }*/
+
+    // == get other things from cache first || get from network if not in the cache
+    event.respondWith(
+        caches.match(event.request).then((response)=>{  // look in the caches for the response
+            return response || fetch(event.request) // if no response - get it from the network
+        })
+    )
+
 })
 
 const promiseWrap = (data)=>{    // return promise that resolves to the rates
