232804601c98df8d6d578d20645dd3cb8c2d7646
Marc-Desktop <marcamillian@gmail.com>
2017-12-20 19:13:28 +0000

    Testing browserify API in the build script

--- node-scripts/build.js
+++ node-scripts/build.js
@@ -1,3 +1,14 @@
+const Browserify = require('browserify')
+
+
 console.log("Starting building things")
+
+// browserify the module test file into a bundle
+
+//const b = Browserify({standAlone: 'something'})
+//b.add('./test.js');
+
+//.pipe(process.stdout)
+
 
 console.log("Ending building things")
