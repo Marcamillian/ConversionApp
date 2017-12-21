74b67858cd79448617eee2ae62ed46efab444dc6
Marc-Desktop <marcamillian@gmail.com>
2017-12-21 14:11:12 +0000

    Added in comments for removing and renaming files

--- node-scripts/build.js
+++ node-scripts/build.js
@@ -15,28 +15,28 @@ const promiseWrap = (data, failed)=>{    // return promise that resolves to the 
 
 
 // remove the old dist folder !! TODO: and its contents - // create the new dist folder & subfolders
-rimraf(dir, ()=>{
-    promiseWrap("deleted"
-    ).then(    // make the new directory
-        ()=>{   fs.mkdirSync(dir)
-                return promiseWrap()},
-        ()=>{throw new Error(`${dir} not created`)}
-    ).then(     // copy the files from src to public folder
-        ()=>{ return promiseWrap(ncp('./src/client', dir)) },
-        ()=>{}
-    ).then(     // browserify the main file
-        ()=>{
-            b.add(`${dir}/main.js`)
-            b.bundle().pipe(fs.createWriteStream(`${dir}/main_bundle1.js`))
-            return promiseWrap()
-        },
-        ()=>{}
-    ).then(
-        ()=>{ rimraf(`${dir}/modules`, ()=>{console.log("done")})}
-    )
-    .catch((err)=>{
-        console.log(`Build abandoned ${err}`)
-    })
+promiseWrap(rimraf(dir, ()=>{}) // delete the old public directory
+).then(    // make the new directory
+    ()=>{   fs.mkdirSync(dir);
+            return promiseWrap()},
+    ()=>{throw new Error(`${dir} not created`)}
+).then(     // copy the files from src to public folder
+    ()=>{ return promiseWrap(ncp('./src/client', dir)) },
+    ()=>{}
+).then(     // browserify the main file
+    ()=>{
+        b.add(`${dir}/main.js`)
+        b.bundle().pipe(fs.createWriteStream(`${dir}/main_bundle1.js`))
+        // fs.rename()
+        // fs.unlink()
+        return promiseWrap()
+    },
+    ()=>{}
+).then(
+    //()=>{ rimraf(`${dir}/modules`, ()=>{console.log("done")})}
+)
+.catch((err)=>{
+    console.log(`Build abandoned ${err}`)
 })
 
 
