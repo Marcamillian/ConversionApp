ad7dd30c1da40591d0952943c494980d17ae7f04
Marc-Desktop <marcamillian@gmail.com>
2017-12-22 10:52:26 +0000

    Using babel in the conversion

--- node-scripts/build.js
+++ node-scripts/build.js
@@ -1,38 +1,46 @@
-const Browserify = require('browserify')
+const browserify = require('browserify')
 const fs = require('fs')
 const rimraf = require('rimraf')
 const ncp = require('ncp')
+const babelify = require('babelify')
 
-const b = Browserify({standalone: 'something'})
-const dir = './public';
+const dest_dir = './public';
+const source_dir = './src/client'
 let dirDeletePromise;
 
 //fs.rename('./node-scripts/test.js', './node-scripts/testrename.js', (err)=>{console.log(`Rename file : ${err}`)})
 
 // remove the directory if it already exists
-new Promise((resolve,reject)=>{ rimraf(dir, resolve)
+new Promise((resolve,reject)=>{ rimraf(dest_dir, resolve)
 }).then(()=>{ // THEN make the new public directory
-    return new Promise((resolve, reject)=>{ fs.mkdir(dir, resolve) })
+    return new Promise((resolve, reject)=>{ fs.mkdir(dest_dir, resolve) })
 }).then(()=>{ // THEN copy across the client files
-    return new Promise((resolve, reject)=>{ ncp('./src/client', dir, resolve)})
-}).then(()=>{ // THEN browserify the main file & write to disk
+    return new Promise((resolve, reject)=>{ ncp(source_dir, dest_dir, resolve)})
+}).then(()=>{ // THEN transpile (babel/browserify) the main file & write to disk
     return new Promise((resolve, reject)=>{
-        let fileWriteOperation = fs.createWriteStream(`${dir}/main_bundle.js`)
+        let fileWriteOperation = fs.createWriteStream(`${dest_dir}/main_bundle.js`)
         
-        b.add(`${dir}/main.js`);
-        b.bundle().pipe(fileWriteOperation)
+        browserify({debug:true})
+            .transform(babelify)
+            .require(`${source_dir}/main.js`, {entry:true})
+            .bundle()
+            .on("error", function(err){console.log("Error: "+ err.message)})
+            .pipe(fileWriteOperation)
 
         fileWriteOperation.on('finish', resolve()) // resolve the promise when it finishes the file write
     })
 // THEN parse the file through babel
+}).then((err, result)=>{ // THEN write the file to dist
+    console.log(`babel parse: ${result}`)
+    return new Promise((resolve, reject)=>{ resolve()})
 }).then(()=>{   // THEN remove the modules directory
-    return new Promise((resolve, reject)=>{ rimraf(`${dir}/modules`, resolve)})
-})/*.then(()=>{ // remove the un-transpiled file 
+    return new Promise((resolve, reject)=>{ rimraf(`${dest_dir}/modules`, resolve)})
+}).then(()=>{ // remove the un-transpiled file 
     return new Promise((resolve, reject)=>{
-        fs.unlink(`${dir}/main.js`, resolve)
+        fs.unlink(`${dest_dir}/main.js`, resolve)
     })
-})*/.then(()=>{ // rename the bundle
-    return new Promise((resolve, reject)=>{ fs.rename(`${dir}/main_bundle.js`, `${dir}/main1.js`, resolve )})
+}).then(()=>{ // rename the bundle
+    return new Promise((resolve, reject)=>{ fs.rename(`${dest_dir}/main_bundle.js`, `${dest_dir}/main.js`, resolve )})
 }).then(
     ()=>{ console.log("Finished the build")},
     (err)=>{console.log(`Couldn't rename: ${err}`)}
